844110eaf5f91c2bea22a9cda5e7a40d74e7fc8b
Arthur Green <arthur@phraction.org>
2017-10-06 14:58:23 +0100

    Add config, manage

---
+++ manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+from app import create_app, db
+from app.models import StarTable
+from flask_script import Manager, Shell
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+
+
+def make_shell_context():
+    '''shell context'''
+    return dict(app=app, db=db, Star=Star)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
