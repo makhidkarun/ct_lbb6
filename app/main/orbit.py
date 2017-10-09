fa0f5ff9ab218c92bf6a789b3fddb3bca8be2aab
Arthur Green <arthur@phraction.org>
2017-10-09 17:00:48 +0100

    orbit.py checking pending debug on SQL query

--- app/main/orbit.py
+++ app/main/orbit.py
@@ -5,48 +5,55 @@ from math import atan2, pi
 from ..models import OrbitTable
 
 LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.ERROR)
+LOGGER.setLevel(logging.DEBUG)
 
 
 class Orbit(object):
     '''Orbit class'''
-    def __init__(self, star, orbit_no=0):
+    def __init__(self, star, orbit_no=None):
         self.star = star
-        self.orbit_no = orbit_no
+        self.orbit_no = None
         self.au = 0
         self.mkm = 0
         self.period = 0
         self.angular_dia_deg = 0
         self.angular_dia_sun = 0
-        self.get_radius()
+        LOGGER.debug('orbit_no = %s', orbit_no)
+        LOGGER.debug('type(orbit_no) = %s', type(orbit_no))
+        self.get_radius(orbit_no)
         self.determine_period()
         self.determine_angular_diameter()
 
     def determine_period(self):
         '''Determine orbital period - need orbit radius, stellar mass'''
-        self.period = (self.au ** 3 / self.star.mass) ** 0.5
-        LOGGER.debug('period = %s', self.period)
+        if self.orbit_no:
+            self.period = (self.au ** 3 / self.star.mass) ** 0.5
+            LOGGER.debug('period = %s', self.period)
 
     def determine_angular_diameter(self):
         '''Determine angular diameter of star as seen from this orbit'''
         # a = 2*arctan(Dstellar / (2 * R))
         # Convert from solar dia to Mkm (Dsun = 1.3914 Mkm)
-        stellar_dia = self.star.radius * 1.3914
-        LOGGER.debug('stellar dia = %s Mkm', stellar_dia)
-        LOGGER.debug('orbital rad = %s Mkm', self.mkm)
-        self.angular_dia_deg = atan2(stellar_dia, self.mkm) * 180 / pi
-        # Sun's angular diameter from earth orbit ~ 0.522 deg
-        self.angular_dia_sun = self.angular_dia_deg / 0.522
-        LOGGER.debug(
-            'angular dia = %s (%sx the sun from earth',
-            self.angular_dia_deg,
-            self.angular_dia_sun)
+        if self.orbit_no:
+            stellar_dia = self.star.radius * 1.3914
+            LOGGER.debug('stellar dia = %s Mkm', stellar_dia)
+            LOGGER.debug('orbital rad = %s Mkm', self.mkm)
+            self.angular_dia_deg = atan2(stellar_dia, self.mkm) * 180 / pi
+            # Sun's angular diameter from earth orbit ~ 0.522 deg
+            self.angular_dia_sun = self.angular_dia_deg / 0.522
+            LOGGER.debug(
+                'angular dia = %s (%sx the sun from earth',
+                self.angular_dia_deg,
+                self.angular_dia_sun)
 
-    def get_radius(self):
+    def get_radius(self, orbit_no):
         '''Get orbit radius from OrbitTable'''
-        details = OrbitTable.query.\
-            filter_by(indx=self.orbit_no).\
-            first()
-        LOGGER.debug('orbit = %s', details)
-        self.au = details.au
-        self.mkm = details.mkm
+        if orbit_no:
+            details = OrbitTable.query.\
+                filter_by(indx=self.orbit_no).\
+                first()
+            LOGGER.debug('orbit details = %s', details)
+            if details:
+                self.orbit_no = orbit_no
+                self.au = details.au
+                self.mkm = details.mkm
